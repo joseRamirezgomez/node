5b938dab14bb3fd52962c9af1df1dd9146752bde
joseRamirezgomez <joseramirezgomez00@gmail.com>
2023-08-10 11:18:34 -0500

    add final_node. falta modificar fondos y colocar la info

---
+++ final_node/src/routes/index.js
@@ -0,0 +1,9 @@
+import { Router } from "express"
+const router = Router()
+router.get('/',(req, res) => res.render('index', {title: 'Inicio'}))
+router.get('/about',(req, res) => res.render('about', {title: 'Quiénes somos'}))
+router.get('/contact',(req, res) => res.render('contact', {title: 'Contactanos'}))
+router.get('/services',(req, res) => res.render('services', {title: 'Servicios'}))
+router.get('/portfolio',(req, res) => res.render('portfolio', {title: 'Portafolio'}))
+router.get('/testimony',(req, res) => res.render('testimony', {title: 'Testimonios'}))
+export default router
