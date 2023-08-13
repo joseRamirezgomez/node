c586dad2d714437ab4ffc624d1bdc819f4e587bd
JoseRamirez <joseramirezgomez00@gmail.com>
2023-08-13 16:55:10 -0500

    proyecto node finalizado

--- final_node/src/routes/index.js
+++ final_node/src/routes/index.js
@@ -1,6 +1,6 @@
 import { Router } from "express"
 const router = Router()
-router.get('/',(req, res) => res.render('index', {title: 'Inicio'}))
+router.get('/',(req, res) => res.render('index', {title: 'One Infinity Code'}))
 router.get('/about',(req, res) => res.render('about', {title: 'Quiénes somos'}))
 router.get('/contact',(req, res) => res.render('contact', {title: 'Contactanos'}))
 router.get('/services',(req, res) => res.render('services', {title: 'Servicios'}))
--- proyecto_node/src/routes/index.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express"
-const router = Router()
-router.get('/',(req, res) => res.render('index', {title: 'este es un index'}))
-router.get('/about',(req, res) => res.render('about', {title: 'Pagina about'}))
-router.get('/contact',(req, res) => res.render('contact', {title: 'Pagina contact me'}))
-export default router
